6286c9b989357e0fd54218e5c35f1104ea196be2
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-07-31 12:34:22 -0500

    Implement post request function for activity class

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,6 +5,18 @@ const fetchCalls = {
       .then(response => response.json())
       .then(data => data)
       .catch(err => console.error(err))
+  },
+
+  postNewData(body) {
+    return fetch(`http://localhost:3001/api/v1/${endPoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+      .then(response => response.json())
+      .then(confirmation => console.log(confirmation));
   }
 
 };
